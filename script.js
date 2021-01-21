6dfa5740378a67069414703d634c807b28a232ea
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-01-21 23:39:19 +0600

    created  and gave functions

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let points = 0;
 
 
+let answer1 = prompt(`What year did the first world war start?`);
+if (answer1=== "1914") {
+   points = points + 5
+};
+
+
 
 alert(`You've earned ` + points + " points");
