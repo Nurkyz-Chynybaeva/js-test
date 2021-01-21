8dcb5c6a2d4dc6335cfd162ad2a78878edf7526c
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-01-21 23:47:18 +0600

    added third let

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 let points = 0;
 
 
-let answer1 = prompt(`What year did the first world war start?`);
-if (answer1=== "1914") {
+let answer1 = prompt(`in which country is the legendary Everest?`);
+if (answer1=== "Nepal"){
+  points = points +5
+};
+
+
+let answer2 = prompt(`What year did the first world war start?`);
+if (answer2=== "1914") {
    points = points + 5
 };
 
 
 
-let answer2 = prompt(`What year did the First World War end?`);
-if (answer2=== "1918") {
+let answer3 = prompt(`What year did the First World War end?`);
+if (answer3=== "1918") {
   points = points + 5
 };
 
