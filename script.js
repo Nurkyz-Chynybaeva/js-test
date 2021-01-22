53d8d9cf4abbc72b78ed9569afdbad348b96d993
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-01-22 14:01:29 +0600

    put everything in order

--- script.js
+++ script.js
@@ -6,122 +6,124 @@ if (answer1=== "Nepal"){
   points = points + 5
 };
 
-let answer10 = prompt(`What year was the collapse of the USSR?`);
-if (answer10=== "1991"){
+
+
+
+
+let answer2= prompt(`The first woman in the world to receive a Nobel Prize?`);
+if (answer2 === "Maria Sklodowska-Curie"){
   points = points + 5
 };
 
-let answer11 =prompt(`How many union republics were part of the USSR?`);
-if (answer11=== "15"){
+
+
+
+let answer3 = prompt(`Who is the last king / queen of Egypt?`);
+if (answer3 === "Cleopatra"){
   points = points + 5
 };
 
-let answer2 = prompt(`What year did the first world war start?`);
-if (answer2=== "1914") {
+
+
+
+let answer4 = prompt(`Who became the first woman to serve as Prime Minister of Great Britain?`);
+if (answer4 === "Margaret Thatcher"){
+  points = points + 5
+};
+
+
+
+
+
+let answer5 = prompt(`What year was the collapse of the USSR?`);
+if (answer5=== "1991"){
+  points = points + 5
+};
+
+let answer6 =prompt(`How many union republics were part of the USSR?`);
+if (answer6=== "15"){
+  points = points + 5
+};
+
+let answer7 = prompt(`What year did the first world war start?`);
+if (answer7=== "1914") {
    points = points + 5
 };
 
 
 
-let answer3 = prompt(`What year did the First World War end?`);
-if (answer3=== "1918") {
+let answer8 = prompt(`What year did the First World War end?`);
+if (answer8=== "1918") {
   points = points + 5
 };
 
 
-let answer4 = prompt(`Aboute how old is the Pyramid of Cheops?`);
-if (answer4=== "4500"){
+let answer9 = prompt(`Aboute how old is the Pyramid of Cheops?`);
+if (answer9=== "4500"){
   points = points + 5
 };
 
-let answer5 = prompt(`How many recognized countries are there in the world?`);
-if (answer5=== "197"){
+let answer10 = prompt(`How many recognized countries are there in the world?`);
+if (answer10=== "197"){
   points = points + 5
 };
 
-let answer7 = prompt(`The most developed country in the world?`);
-if (answer7=== "Switzerland"){
+let answer11 = prompt(`The most developed country in the world?`);
+if (answer11=== "Switzerland"){
   points = points + 5
 };
 
-let answer8 =prompt(`Which country is the most economically successful country in the European Union?`);
-if (answer8=== "Germany"){
+let answer12 =prompt(`Which country is the most economically successful country in the European Union?`);
+if (answer12=== "Germany"){
   points = points + 5
 };
 
-let answer9 = prompt(`Which is the most developed country in Asia`);
-if (answer9 === "Japan"){
-  points = points + 5
-};
-
-
-let answer6 = prompt (`Structural and functional elementary unit of the structure and life of all organisms?`);
-if (answer6=== "cell"){
-  points = points + 5
-};
-
-let answer12 = prompt(`Which animal is the fastest animal in the world?`)
-if (answer12=== "cheetah"){
-  points = points + 5
-};
-
-let answer13 =prompt(`Цhat is the largest animal in the world?`);
-if (answer13=== "blue whale"){
+let answer13 = prompt(`Which is the most developed country in Asia`);
+if (answer13 === "Japan"){
   points = points + 5
 };
 
 
-let answer14 = prompt(`What is the oldest city in the world?`);
-if (answer14 === "Jericho"){
+let answer14 = prompt (`Structural and functional elementary unit of the structure and life of all organisms?`);
+if (answer14=== "cell"){
+  points = points + 5
+};
+
+let answer15 = prompt(`Which animal is the fastest animal in the world?`)
+if (answer15=== "cheetah"){
+  points = points + 5
+};
+
+let answer16 =prompt(`Цhat is the largest animal in the world?`);
+if (answer16=== "blue whale"){
   points = points + 5
 };
 
 
-let answer15 = prompt(`Which region is the largest region in terms of area in Kyrgyzstan?`);
-if (answer15 === "Naryn"){
+let answer17 = prompt(`What is the oldest city in the world?`);
+if (answer17 === "Jericho"){
   points = points + 5
 };
 
 
-let answer16 =prompt(`Which is the smallest region in Kyrgyzstan?`);
-if (answer16=== "Talas"){
-  points = points + 5
-};
-
-let answer17= prompt(`What is the highest mountain peak in Kyrgyzstan?`);
-if (answer17 === "Lenin"){
-  points = points = 5
-};
-
-
-let answer18 = prompt(`Which epic is the greatest epic in the world?`);
-if (answer18 === "Manas"){
+let answer18 = prompt(`Which region is the largest region in terms of area in Kyrgyzstan?`);
+if (answer18 === "Naryn"){
   points = points + 5
 };
 
 
-let answer19 = prompt(`Who became the first woman to serve as Prime Minister of Great Britain?`);
-if (answer19 === "Margaret Thatcher"){
+let answer19 =prompt(`Which is the smallest region in Kyrgyzstan?`);
+if (answer19=== "Talas"){
   points = points + 5
 };
 
-let answer20 = prompt(`Who is the last king / queen of Egypt?`);
-if (answer20 === "Cleopatra"){
+
+let answer20 = prompt(`Which epic is the greatest epic in the world?`);
+if (answer20 === "Manas"){
   points = points + 5
 };
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-alert(`You've earned ` + points + " points");
+console.log(`You've earned ` + points + " points");
